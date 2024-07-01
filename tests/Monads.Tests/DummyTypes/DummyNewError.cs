15f7724a0fa66d5fb1d70274d396ff7b487a30fb
Tim Ray <timothy@timray.dev>
2024-07-01 16:01:55 -0500

    Add initial unit tests for 'Result' monad

---
+++ tests/Monads.Tests/DummyTypes/DummyNewError.cs
@@ -0,0 +1,7 @@
+namespace Monads.Tests.DummyTypes;
+
+public class DummyNewError
+{
+    public required string Message { get; init; }
+    public int Count { get; init; }
+}
---
+++ tests/Monads.Tests/DummyTypes/DummyNewValue.cs
@@ -0,0 +1,6 @@
+namespace Monads.Tests.DummyTypes;
+
+public class DummyNewValue
+{
+    public required string NameAllCaps { get; init; }
+}
