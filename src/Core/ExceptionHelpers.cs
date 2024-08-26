177332418e9f13f02d77f12402c39175a233fee6
Tim Ray <timothy@timray.dev>
2024-08-25 21:38:03 -0500

    Remove Analyzers.Sample assembly
    The sample assembly usefull during the early development phase, but it is no longer needed.

---
+++ src/Core/ExceptionHelpers.cs
@@ -0,0 +1,17 @@
+namespace NetFunctional.Core;
+
+public static class ExceptionHelpers
+{
+    public static TException TryToConstructException<TException>(object[]? constructorArgs = default)
+        where TException : Exception
+    {
+        if (Activator.CreateInstance(typeof(TException), constructorArgs ?? []) is not TException activatedException)
+        {
+            throw new InvalidOperationException(
+                $"No matching constructor found for the given exception type '{typeof(Exception).FullName}'."
+            );
+        }
+
+        return activatedException;
+    }
+}
