e41a3fb943d13655f90b9c16470a287d110b46ff
Tim Ray <timothy@timray.dev>
2025-05-29 21:56:41 -0500

    fix: replace deprecated method calls

--- src/Types/Option/OptionTypeExtensions.cs
+++ src/Types/Option/OptionTypeExtensions.cs
@@ -5,20 +5,12 @@ namespace DotNetCoreFunctional.Option;
 
 public static class OptionTypeExtensions
 {
-    public static Option<T> ToOption<T>(this T? value)
-        where T : notnull
-    {
-        return Option.Return(value);
-    }
-
-    public static Option<T> ToSome<T>(this T value)
-        where T : notnull => Option.Some(value);
 
     public static IEnumerable<Option<T>> ToOptionEnumerable<T>(this IEnumerable<T?> enumerable)
         where T : notnull => enumerable.Select(val => val.ToOption());
 
     public static IEnumerable<Option<T>> ToSomeEnumerable<T>(this IEnumerable<T> enumerable)
-        where T : notnull => enumerable.Select(val => val.ToSome());
+        where T : notnull => enumerable.Select(val => val.ToOption());
 
     public static Option<TCast> CastToOption<T, TCast>(this T? value)
         where TCast : notnull
